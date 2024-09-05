dd4fad4a9e389bf41468e98e3042bca6559f757d
enoc <keepcoding@mflapique.com>
2024-09-05 18:13:17 +0200

    Segundo ejercicio: #02

---
+++ ejercicio2.js
@@ -0,0 +1,19 @@
+const calcularPromedio = (listaNums) => {
+
+    let sumaNums = 0
+
+    /*
+    EL bug estaba en el <= de la longitud de la lista puesto que tiene que ser length-1 el valor de i maximo
+    */
+
+    for (let i = 0; i < listaNums.length; i++){
+        sumaNums += listaNums[i]
+    }
+
+    const promedio = sumaNums / listaNums.length
+    return promedio
+}
+const listaNumeros = [1,2,3,4]
+const promedioListaNumeros = calcularPromedio(listaNumeros)
+
+console.log(promedioListaNumeros)
